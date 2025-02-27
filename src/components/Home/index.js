7ef0ece03902e295a483dcc9feb5b7078aaaf00f
Jagadeesh-Kmr <jagadeesh.1724@gmail.com>
2025-02-27 20:27:43 +0530

    Updated project

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -234,7 +234,7 @@ const Home = () => {
                     From developing sleek UI components to integrating REST APIs, I focus on delivering high-quality, optimized, and accessible web solutions.</p>
                     </div>
                     <div className="resume-div">
-                    <a href="https://drive.google.com/file/d/1MN_5UVTPRVeexq85QIBfCfO4_a4bwidA/view?usp=sharing" 
+                    <a href="https://drive.google.com/file/d/1sXVdbv7Oby_Ri3nkRrZAB426AMm9Tdlo/view?usp=sharing" 
                     target="_blank"
                     rel="noopener noreferrer"
                     className="resume-link">
